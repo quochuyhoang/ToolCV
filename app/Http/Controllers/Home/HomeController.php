62ffd7250623cd20b027261e13d2b1cd6654ece3
Ho Hong Son <son.bjerg@gmail.com>
2019-05-23 20:25:44 +0700

    third

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -10,11 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
- public function __construct()
-	{
+//  public function __construct()
+// 	{
 
-		$this->middleware('auth:web')->only('index');
-    }
+// 		$this->middleware('auth:web')->only('index');
+//     }
 	
 	// public function index(){
 
--- routes/web.php
+++ routes/web.php
@@ -228,6 +228,6 @@ Route::prefix('home')->group(function (){
 
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
